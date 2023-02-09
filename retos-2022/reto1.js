4d408c930b41e998c97ca1f95a42707817712283
agustin <agustinsanchezx@gmail.com>
2023-02-09 10:44:15 -0300

    add reto1

--- retos-2022/reto1.js
+++ retos-2022/reto1.js
@@ -1,1 +1,47 @@
-console.log('hola')
+// Este año los elfos han comprado una máquina que envuelve regalos.
+// Pero… ¡no viene programada! Necesitamos crear un algoritmo que le ayude en la tarea.
+
+// A la máquina se le pasa un array con los regalos. Cada regalo es un string. 
+// Necesitamos que la máquina envuelva cada regalo en papel de regalo y lo coloque en un array de regalos envueltos.
+
+// El papel de regalo es el símbolo * y para envolver un regalo se coloca el símbolo * 
+// de forma que rodee totalmente al string por todos los lados.
+//  Por ejemplo:
+
+// const gifts = ['cat', 'game', 'socks']
+// const wrapped = wrapping(gifts)
+
+// console.log(wrapped)
+// /* [
+//   "*****\n*cat*\n*****",
+//   "******\n*game*\n******",
+//   "*******\n*socks*\n*******"
+// ] */
+
+// Como ves, el papel de regalo envuelve el string.
+// Por arriba y por abajo, para no dejar ningún hueco, 
+// las esquinas también están cubiertas por el papel de regalo.
+
+// Nota: El carácter \n representa un salto de línea.
+
+// ¡Ojo! Asegúrate que pones el número correcto de * para envolver completamente el string. Pero no demasiados. Sólo los necesarios para cubrir el string.
+
+// Ah, y no modifiques (mutes) el array original.
+
+function wrapping(gifts) {
+
+    const newArray = []
+
+    gifts.forEach((gift) => {
+        const aterisc = '*'.repeat(gift.length + 2)
+        const newGift = `${aterisc}\n*${gift}*\n${aterisc}`
+        newArray.push(newGift)      
+    })
+
+    return newArray;
+}
+
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
